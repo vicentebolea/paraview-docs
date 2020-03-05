a1bb3ffe968cf9245e79de864f91019c032fc2a9
buildbot <buildbot@kwrobot02>
2020-03-05 08:12:00 -0500

    update docs for '5.8.0-288-gdeade123e6' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-286-gb3d3b75e90',
+    VERSION: '5.8.0-288-gdeade123e6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
