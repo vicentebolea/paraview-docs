cf6ac18650810f2db0f9a6989427407e350725c2
buildbot <buildbot@kwrobot02>
2020-03-24 01:43:24 -0400

    update docs for '5.8.0-402-g32669c4e4e' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-398-g4cd192b37c',
+    VERSION: '5.8.0-402-g32669c4e4e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
