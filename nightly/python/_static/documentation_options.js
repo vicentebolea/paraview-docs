8c133edabdb40b84b3db04a46d718ce43629040b
buildbot <buildbot@kwrobot02>
2020-04-01 17:52:31 -0400

    update docs for '5.8.0-454-ge943c38d1d' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-449-ga1859ca70d',
+    VERSION: '5.8.0-454-ge943c38d1d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
