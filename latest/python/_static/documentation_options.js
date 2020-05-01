c67ac97fc2ffd242304c31d97d35ead39a5e7524
buildbot <buildbot@kwrobot02>
2020-04-30 22:42:10 -0400

    update docs for '5.8.0-87-g21506800cc' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-85-ge4d7d89e3d',
+    VERSION: '5.8.0-87-g21506800cc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
