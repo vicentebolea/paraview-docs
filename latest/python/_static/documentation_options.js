041ab5d921edf079fae4b9261f687e5ceef394e5
buildbot <buildbot@kwrobot02>
2020-05-11 20:41:58 -0400

    update docs for '5.8.0-89-g8e58b76b1e' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-87-g21506800cc',
+    VERSION: '5.8.0-89-g8e58b76b1e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
