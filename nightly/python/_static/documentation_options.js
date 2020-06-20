56410323e337095c09192f7751ddd2c96c1eecd0
buildbot <buildbot@kwrobot02>
2020-06-20 14:33:03 -0400

    update docs for '5.8.1-RC1-827-g9964927217' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.1-RC1-824-ga5e56e85cf',
+    VERSION: '5.8.1-RC1-827-g9964927217',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
